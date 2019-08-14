e42e5662a6b78c8f5237086d501265ed9d083b5b
jvanheld <Jacques.van-Helden@univ-amu.fr>
2019-08-14 16:36:27 +0200

    specified default libraries in the signature of LoadLibraries()

--- cisreg-GWAS_wf/R/load_libraries.R
+++ cisreg-GWAS_wf/R/load_libraries.R
@@ -1,34 +1,51 @@
-LoadLibraries <- function(cran.libraries.to.install = NULL,
-                          bioconductor.libraries.to.install = NULL,
-                          github.libraries.to.install = NULL,
+
+#' @title Load required libraries and install them if required
+#' @author Yvon Mbouamboua and Jacques van Helden
+#' @description Load the libraries required for cisreg-GWAS from CRAN + BioConductor + github
+#' @param cran.libraries.to.install a vector with the names of CRAN libraries to install (default list is used if not specified)
+#' @param bioconductor.libraries.to.install a vector with names of the BioConductor libraries to instal (default list is used if not specified)
+#' @param github.libraries.to.install a named vector with the names of libraries to install from github (names of the vector elements), and the corresponding accounts (values of the vector elements)
+#' @param install.if.required=TRUE automatically install the missing libraries
+#' 
+#' @export
+LoadLibraries <- function(cran.libraries.to.install=c("yaml",
+                                                      "dplyr",   
+                                                      "devtools",
+                                                      "ggplot2", 
+                                                      "gridExtra",
+                                                      "cowplot",
+                                                      "scales",
+                                                      "tidyr",
+                                                      #"VSE",
+                                                      "DiagrammeR",
+                                                      "VennDiagram",
+                                                      "jsonlite",
+                                                      "httr",
+                                                      "xml2",
+                                                      "RCurl",
+                                                      "data.table"),
+                          bioconductor.libraries.to.install = c(
+                            "biomaRt",
+                            "GenomicRanges",
+                            "ChIPpeakAnno",
+                            "ChIPseeker",
+                            "TxDb.Hsapiens.UCSC.hg38.knownGene",
+                            "ReactomePA",
+                            "enrichplot",
+                            #"rGREAT",
+                            "XGR",  ## For xEnrichSNPs and related graph representations
+                            #"FunciSNP",
+                            "TissueEnrich",
+                            "grImport2",
+                            "DOSE"),
+                          github.libraries.to.install = c("ReMapEnrich" = "remap-cisreg"),
                           install.if.required = TRUE) {
   
   
   
   #### Load CRAN packages (install them if required) ####
   message("Loading required libraries")
-  if (is.null(cran.libraries.to.install)) {
-    cran.libraries.to.install <- 
-      c("yaml",
-        "dplyr",   
-        "devtools",
-        "ggplot2", 
-        "gridExtra",
-        "cowplot",
-        "scales",
-        "tidyr",
-        #"VSE",
-        "DiagrammeR",
-        "VennDiagram",
-        "jsonlite",
-        "httr",
-        "xml2",
-        "RCurl",
-        "data.table"
-        # "gProfileR"
-      )     
-  }    
-  
+
   message("Loading CRAN libraries")
   for (lib in cran.libraries.to.install) {
     if (require(lib, character.only = TRUE, quietly = TRUE)) {
@@ -43,23 +60,6 @@ LoadLibraries <- function(cran.libraries.to.install = NULL,
   }
   
   #### Load Bioconductor packages (install them if required) ####
-  if (is.null(bioconductor.libraries.to.install)) {
-    bioconductor.libraries.to.install <- c(
-      "biomaRt",
-      "GenomicRanges",
-      "ChIPpeakAnno",
-      "ChIPseeker",
-      "TxDb.Hsapiens.UCSC.hg38.knownGene",
-      "ReactomePA",
-      "enrichplot",
-      #"rGREAT",
-      "XGR",  ## For xEnrichSNPs and related graph representations
-      #"FunciSNP",
-      "TissueEnrich",
-      "grImport2",
-      "DOSE"
-    )
-  }  
   message("Loading Bioconductor libraries")
   for (lib in bioconductor.libraries.to.install) {
     if (!require(lib, character.only = TRUE, quietly = TRUE)) {
@@ -83,10 +83,6 @@ LoadLibraries <- function(cran.libraries.to.install = NULL,
   #### Load some specific R packages from github with devtools ####
   
   ## For github libraries we need to know the account for each package -> we encode this as a named vector
-  if (is.null(github.libraries.to.install)) {
-    message("Temporarily disactivating the installatoion of remap-cisreg/ReMapEnrich due to system-wise library dependency of RMySQL")
-#    github.libraries.to.install <- c("ReMapEnrich" = "remap-cisreg")
-  }
   message("Loading github libraries")
   for (lib in names(github.libraries.to.install)) {
     if (require(lib, character.only = TRUE, quietly = TRUE)) {
